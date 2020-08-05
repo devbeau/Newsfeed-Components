13cc6df2b8e3fe72e2061cab246f3437fd04c293
devbeau <48367657+devbeau@users.noreply.github.com>
2020-08-05 14:39:20 -0400

    mvpish

--- components/Article.js
+++ components/Article.js
@@ -88,29 +88,68 @@ const data = [
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   }
 ];
+  // Step 1: Write a component called 'articleMaker' to create an article.
+  // Your component is a function that takes an article object as its only argument,
+  // and returns a DOM node looking like the one below:
 
-/*
-  Step 1: Write a component called 'articleMaker' to create an article.
-  Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
+    function articleMaker(articleObj){
 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
+      function createNewElement(htmlElement, className = 0, textContent = 0){
+        className = className || 0;
+        textContent = textContent || 0;
+        let newElement = document.createElement(htmlElement);
+        className !== 0 ? newElement.classList.add('className') : newElement;
+        textContent !== 0 ? newElement.textContent = textContent : textContent = null;
+        return newElement;
+      }
+     
+      let article = createNewElement('div', 'article');
+      let header = createNewElement('h2', 0 ,articleObj.title);
+      let date = createNewElement('p', "date", articleObj.date);
+      let pgraphOne = createNewElement('p', 0, articleObj.firstParagraph);
+      let pgraphTwo = createNewElement('p', 0, articleObj.secondParagraph);
+      let pgraphThree = createNewElement('p', 0, articleObj.thirdParagraph);
+      let expandBtn = createNewElement('span', 'expandButton', '+');
 
-    {three separate paragraph elements}
+      console.log(article)
 
-    <span class="expandButton">+</span>
-  </div>
+      article.appendChild(header);
+      article.appendChild(date);
+      article.appendChild(pgraphOne);
+      article.appendChild(pgraphTwo);
+      article.appendChild(pgraphThree);
 
-  Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
-  This listener should toggle the class 'article-open' on div.article.
+      expandBtn.addEventListener('click', (event) => {
+        article.classList.toggle('article-open');
+        console.log("articleMaker -> event", event);
+      });
+      
+      return article;
+    }
 
-  Step 3: Don't forget to return something from your function!
+    let container = document.querySelector('.articles');
 
-  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
+    data.forEach((article) =>{
+      const newArticle = articleMaker(article);
+      container.appendChild(newArticle);
+    });
+  // <div class="article">
+  //   <h2>{title of the article}</h2>
+  //   <p class="date">{date of the article}</p>
 
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
-  Refresh the page to see the new article.
-*/
+  //   {three separate paragraph elements}
+
+  //   <span class="expandButton">+</span>
+  // </div>
+
+  // Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+  // This listener should toggle the class 'article-open' on div.article.
+
+  // Step 3: Don't forget to return something from your function!
+
+  // Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+  // to create a div.article element and append it to the DOM inside div.articles (see index.html).
+
+  // Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  // Refresh the page to see the new article.
+
